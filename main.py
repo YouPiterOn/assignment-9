682a0695e38820d6bab4ab9dffcf9f79f0a61d6a
Varvara Chornomorets <vchornomorets@kse.org.ua>
2023-01-26 13:11:48 +0200

    added mean scores comparison to the 1st task

--- main.py
+++ main.py
@@ -15,11 +15,16 @@ def imdb_scores_hist():
     # print(movies.loc[:, "type"])
 
     figure, axes = plt.subplots(2)
-    imdb_scores = shows.loc[:, "imdb_score"].dropna()
-    print(imdb_scores)
-    axes[0].hist(imdb_scores, np.arange(0, 10.2, 0.2))
-    imdb_scores = movies.loc[:, "imdb_score"]
-    axes[1].hist(imdb_scores, np.arange(0, 10.2, 0.2))
+    imdb_scores_shows = shows.loc[:, "imdb_score"].dropna()
+    # print(imdb_scores_shows)
+    axes[0].hist(imdb_scores_shows, np.arange(0, 10.2, 0.2))
+    imdb_scores_movies = movies.loc[:, "imdb_score"]
+    axes[1].hist(imdb_scores_movies, np.arange(0, 10.2, 0.2))
+
+    mean_score_movies = imdb_scores_movies.mean()
+    mean_score_shows = imdb_scores_shows.mean()
+    print(f"Mean score for movies is {mean_score_movies}, and for shows is {mean_score_shows}")
+
     plt.show()
 
 
