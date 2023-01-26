bd0fa0fdfa119a569cad245821cf06ec0e17d305
Varvara Chornomorets <vchornomorets@kse.org.ua>
2023-01-26 11:03:19 +0200

    minor fixes 1st task (step is exactly 0.2, dropped NA)

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 # Побудуйте окремі гістограмми розподілу фільмів
@@ -7,17 +8,19 @@ import matplotlib.pyplot as plt
 # У кого вища середня оцінка? - 0.5 бали;
 def imdb_scores_hist():
     title = pd.read_csv("titles.csv")
-    print(title)
-    shows = title[title["type"] == 'SHOW']
-    print(shows.loc[:, "type"])
+    # print(title)
+    shows = title[title["type"] == 'SHOW'].dropna()
+    # print(shows.loc[:, "type"])
     movies = title[title["type"] == 'MOVIE']
-    print(movies.loc[:, "type"])
+    # print(movies.loc[:, "type"])
 
     figure, axes = plt.subplots(2)
-    imdb_scores = shows.loc[:, "imdb_score"]
-    axes[0].hist(imdb_scores, 50)
+    imdb_scores = shows.loc[:, "imdb_score"].dropna()
+    print(imdb_scores)
+    axes[0].hist(imdb_scores, np.arange(0, 10.2, 0.2))
     imdb_scores = movies.loc[:, "imdb_score"]
-    axes[1].hist(imdb_scores, 50)
+    axes[1].hist(imdb_scores, np.arange(0, 10.2, 0.2))
     plt.show()
+
 
 imdb_scores_hist()
